acc948f3268f5d08d59e34479eb7b5546acbf6e2
KevinCabrera04 <kevincabrera35@aragon.unam.mx>
2026-02-25 10:02:39 -0600

    operadores logicos

---
+++ app4.js
@@ -0,0 +1,10 @@
+//operadores logicos
+
+// &&:  si los dos son verdaderos, el resultado es verdadero
+// ||: si uno de los dos es verdadero, el resultado es verdadero
+// ! : niega el valor, si es verdadero, lo convierte en falso, y viceversa
+
+var compara = 10 > 8 && 10 == 10
+console.log(compara);
+var compara = true && true;
+console.log("Comparacion &&: "+compara);
