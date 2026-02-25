42ce16a21136b9d4a7c2cab08090e2f3bae668a2
KevinCabrera04 <kevincabrera35@aragon.unam.mx>
2026-02-25 09:50:52 -0600

    sumas y restas

--- app2.js
+++ app2.js
@@ -1,7 +1,9 @@
 
+
 var dato1 = parseInt( promt("Ingresa Numero 1"));
 var dato2 = parseInt( promt("Ingresa Numero 2"));
 console.log(typeof dato1)
+
 
 // var rea= dato1 + dato2;
 // console.log(res)
---
+++ app3.js
@@ -0,0 +1,7 @@
+var dato1 = parseInt( promt("Ingresa Numero 1"));
+var dato2 = parseInt( promt("Ingresa Numero 2"));
+console.log("operaciones basicas\n");
+console.log("Los datos son:\n dato1="+dato1+"\nDato2="+dato2);
+
+console.log("Suma:"+dato1+"+"+dato2+"="+(dato1+dato2));
+console.log("resta:"+dato1+"-"+dato2+"="+(dato1-dato2));
