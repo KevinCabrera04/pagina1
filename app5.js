caf48739ad15b571627b18e4f912e3b30e2796cb
KevinCabrera04 <kevincabrera35@aragon.unam.mx>
2026-02-25 10:17:15 -0600

    op de control

---
+++ app5.js
@@ -0,0 +1,14 @@
+//operadores de control
+
+// if: si la condicion es verdadera, se ejecuta el bloque de codigo
+// else: si la condicion es falsa, se ejecuta el bloque de codigo
+// else if: si la condicion es falsa, se evalua otra condicion
+
+let mensaje = promt("Ingresa tu pass");
+if (mensaje == "1234"){
+    console.log("Excelente");
+}else{
+    console.log("Nel, esa no es");
+}
+
+console.log("Termina el programa");
