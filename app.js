f9c6aeac10b6ecdb15a94e9fdcb0f47faebd59ec
KevinCabrera04 <kevincabrera35@aragon.unam.mx>
2026-02-25 09:50:35 -0600

    op basicas

---
+++ app.js
@@ -0,0 +1,14 @@
+
+/*
+var dato = 2;
+var dato2 =2;
+
+var resultado = dato + dato2;
+console.log (resultado); 
+*/
+
+var dato = 'hola';
+var dato2 = 'mundo';
+var dato3 = ' ';
+var res = dato + dato3 + dato2;
+console.log (resu); 
